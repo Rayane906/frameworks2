ba417bf022baab7e9f9c46d8dbdabfe036b46d0e
Arthur-Romero <arthurmcnr11@gmail.com>
2022-08-09 18:03:38 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function home(){
+	document.getElementById('page-01').className="visivel";
+	document.getElementById('page-02').className="invisivel";
+	document.getElementById('page-03').className="invisivel";
+	document.getElementById('page-04').className="invisivel";
+}
+
+function hometopageone(){
+	document.getElementById('page-01').className="invisivel";
+	document.getElementById('page-02').className="visivel";
+}
+
+function hometopagetwo(){
+	document.getElementById('page-01').className="invisivel";
+	document.getElementById('page-03').className="visivel";
+}
+
+function hometopagetree(){
+	document.getElementById('page-01').className="invisivel";
+	document.getElementById('page-04').className="visivel";
+}
